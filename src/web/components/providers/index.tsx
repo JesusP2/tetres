e7ce9fdd8d0367d612ba8910d72d4e3392b69f61
JesusP2 <jesus_perez99@hotmail.com>
2025-06-09 00:44:47 -0600

    fixed navigation issue

--- src/web/components/providers/index.tsx
+++ src/web/components/providers/index.tsx
@@ -31,7 +31,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
         hooks={hooks}
         mutators={mutators}
         Link={NavLink}
-        navigate={navigate}
+        navigate={(href) => navigate({ to: href })}
         magicLink
         passkey
         providers={['google']}
