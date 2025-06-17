e5e458e27d2a0e15ef5d1e14e0f5774f15c2a53a
JesusP2 <jesus_perez99@hotmail.com>
2025-06-17 04:29:58 -0600

    changes

--- src/web/lib/auth-client.ts
+++ src/web/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { passkeyClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
 
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_CLIENT_APP_URL,
+  baseURL: import.meta.env.BASE_URL,
   plugins: [passkeyClient()],
 });
 export const { useSession } = authClient;
